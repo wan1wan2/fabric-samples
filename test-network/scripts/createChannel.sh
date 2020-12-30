65893ba0f92cf1e08bdadf94cd124e539c0bba39
jiajun.wang <jiajun.wang@yungoal.com>
2020-12-30 14:43:46 +0800

    change create channel script

--- test-network/scripts/createChannel.sh
+++ test-network/scripts/createChannel.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source scriptUtils.sh
 
 CHANNEL_NAME="$1"
 DELAY="$2"
@@ -23,25 +22,29 @@ createChannelTx() {
 	set -x
 	configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/${CHANNEL_NAME}.tx -channelID $CHANNEL_NAME
 	res=$?
-	{ set +x; } 2>/dev/null
+	set +x
 	if [ $res -ne 0 ]; then
-		fatalln "Failed to generate channel configuration transaction..."
+		echo "Failed to generate channel configuration transaction..."
+		exit 1
 	fi
+	echo
 
 }
 
-createAnchorPeerTx() {
+createAncorPeerTx() {
 
 	for orgmsp in Org1MSP Org2MSP; do
 
-	infoln "Generating anchor peer update transaction for ${orgmsp}"
+	echo "#######    Generating anchor peer update transaction for ${orgmsp}  ##########"
 	set -x
 	configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/${orgmsp}anchors.tx -channelID $CHANNEL_NAME -asOrg ${orgmsp}
 	res=$?
-	{ set +x; } 2>/dev/null
+	set +x
 	if [ $res -ne 0 ]; then
-		fatalln "Failed to generate anchor peer update transaction for ${orgmsp}..."
+		echo "Failed to generate anchor peer update transaction for ${orgmsp}..."
+		exit 1
 	fi
+	echo
 	done
 }
 
@@ -55,13 +58,15 @@ createChannel() {
 		set -x
 		peer channel create -o localhost:7050 -c $CHANNEL_NAME --ordererTLSHostnameOverride orderer.example.com -f ./channel-artifacts/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block --tls --cafile $ORDERER_CA >&log.txt
 		res=$?
-		{ set +x; } 2>/dev/null
+		set +x
 		let rc=$res
 		COUNTER=$(expr $COUNTER + 1)
 	done
 	cat log.txt
 	verifyResult $res "Channel creation failed"
-	successln "Channel '$CHANNEL_NAME' created"
+	echo
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
 }
 
 # queryCommitted ORG
@@ -76,11 +81,12 @@ joinChannel() {
     set -x
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block >&log.txt
     res=$?
-    { set +x; } 2>/dev/null
+    set +x
 		let rc=$res
 		COUNTER=$(expr $COUNTER + 1)
 	done
 	cat log.txt
+	echo
 	verifyResult $res "After $MAX_RETRY attempts, peer0.org${ORG} has failed to join channel '$CHANNEL_NAME' "
 }
 
@@ -95,50 +101,55 @@ updateAnchorPeers() {
     set -x
 		peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls --cafile $ORDERER_CA >&log.txt
     res=$?
-    { set +x; } 2>/dev/null
+    set +x
 		let rc=$res
 		COUNTER=$(expr $COUNTER + 1)
 	done
 	cat log.txt
   verifyResult $res "Anchor peer update failed"
-  successln "Anchor peers updated for org '$CORE_PEER_LOCALMSPID' on channel '$CHANNEL_NAME'"
+  echo "===================== Anchor peers updated for org '$CORE_PEER_LOCALMSPID' on channel '$CHANNEL_NAME' ===================== "
   sleep $DELAY
+  echo
 }
 
 verifyResult() {
   if [ $1 -ne 0 ]; then
-    fatalln "$2"
+    echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!"
+    echo
+    exit 1
   fi
 }
 
 FABRIC_CFG_PATH=${PWD}/configtx
 
 ## Create channeltx
-infoln "Generating channel create transaction '${CHANNEL_NAME}.tx'"
+echo "### Generating channel create transaction '${CHANNEL_NAME}.tx' ###"
 createChannelTx
 
 ## Create anchorpeertx
-infoln "Generating anchor peer update transactions"
-createAnchorPeerTx
+echo "### Generating anchor peer update transactions ###"
+createAncorPeerTx
 
 FABRIC_CFG_PATH=$PWD/../config/
 
 ## Create channel
-infoln "Creating channel ${CHANNEL_NAME}"
+echo "Creating channel "$CHANNEL_NAME
 createChannel
 
 ## Join all the peers to the channel
-infoln "Join Org1 peers to the channel..."
+echo "Join Org1 peers to the channel..."
 joinChannel 1
-infoln "Join Org2 peers to the channel..."
+echo "Join Org2 peers to the channel..."
 joinChannel 2
 
 ## Set the anchor peers for each org in the channel
-infoln "Updating anchor peers for org1..."
+echo "Updating anchor peers for org1..."
 updateAnchorPeers 1
-infoln "Updating anchor peers for org2..."
+echo "Updating anchor peers for org2..."
 updateAnchorPeers 2
 
-successln "Channel successfully joined"
+echo
+echo "========= Channel successfully joined =========== "
+echo
 
 exit 0
