b494b852d438399ef9c254b63e8399d9ffa15ad1
jiajun.wang <jiajun.wang@yungoal.com>
2021-01-06 11:55:58 +0800

    add create asset test cases

--- asset-transfer-secured-agreement/chaincode-java/src/test/java/org/example/fabric/samples/securedagreement/AssetTransferTest.java
+++ asset-transfer-secured-agreement/chaincode-java/src/test/java/org/example/fabric/samples/securedagreement/AssetTransferTest.java
@@ -2,6 +2,7 @@ package org.example.fabric.samples.securedagreement;
 
 import org.hyperledger.fabric.contract.ClientIdentity;
 import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.shim.ChaincodeException;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,14 +11,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 public class AssetTransferTest {
     @Nested
     class InvokeWriteTransaction {
+
+        @Test
+        public void createAssetWithoutTransientMap() {
+            AssetTransfer contract = new AssetTransfer();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+            Map<String, byte[]> transientMap = new HashMap<>();
+            when(stub.getTransient()).thenReturn(transientMap);
+
+            Throwable thrown = catchThrowable(()-> contract.createAsset(ctx, "asset1", "public description text"));
+
+            assertThat(thrown).isInstanceOf(ChaincodeException.class).hasNoCause();
+            assertThat(((ChaincodeException) thrown).getPayload()).isEqualTo("INCOMPLETE_INPUT".getBytes());
+        }
+
+        @Test
+        public void createAssetWithDifferentOrg() {
+            AssetTransfer contract = new AssetTransfer();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+            when(stub.getMspId()).thenReturn("whatever");
+            ClientIdentity ci = mock(ClientIdentity.class);
+            when(ci.getId()).thenReturn(testOrg1Client);
+            when(ci.getMSPID()).thenReturn(testOrgOneMSP);
+            when(ctx.getClientIdentity()).thenReturn(ci);
+
+            Map<String, byte[]> transientMap = new HashMap<>();
+            AssetForSale assetForSale = new AssetForSale("asset1", "asset_properties"
+                    , "blue", 16, "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3");
+            transientMap.put("asset_properties", assetForSale.serialize());
+            when(stub.getTransient()).thenReturn(transientMap);
+
+            Throwable thrown = catchThrowable(()-> contract.createAsset(ctx, "asset1", "public description text"));
+            assertThat(thrown).isInstanceOf(ChaincodeException.class).hasNoCause();
+            assertThat(((ChaincodeException) thrown).getPayload()).isEqualTo("INVALID_ACCESS".getBytes());
+        }
 
         @Test
         public void createAssetShouldBeSuccess() {
@@ -47,6 +85,6 @@ public class AssetTransferTest {
         return new Asset("asset1", testOrgOneMSP, "public description text");
     }
 
-    private static String testOrg1Client = "testOrg1User";
-    private static String testOrgOneMSP = "TestOrg1";
+    private static final String testOrg1Client = "testOrg1User";
+    private static final String testOrgOneMSP = "TestOrg1";
 }
